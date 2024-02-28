fbe6a0364783e39b7f455002921b1984ac5d3105
Meraklı Mühendis <myagizorhan@hotmail.com>
2024-02-28 23:47:40 +0300

    Bilgi Okuma Eklendi.

--- database_models/main.py
+++ database_models/main.py
@@ -37,3 +37,11 @@ async def create_user(user: UserBase, db: db_dependency):
     db_user = models.User(**user.dict())
     db.add(db_user)
     db.commit()
+
+
+@app.get("/users/{user_id}", status_code = status.HTTP_200_OK)
+async def read_user(user_id: int, db: db_dependency):
+    user = db.query(models.User).filter(models.User.ID == user_id).first()
+    if user is None:
+        raise HTTPException(status_code = 404, detail = "User not found")
+    return user
